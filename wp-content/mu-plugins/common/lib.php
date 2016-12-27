c061b557f442404e6fd4c15f36db81a055d3f9fc
Sara Arjona <sarjona@xtec.cat>
2016-12-27 10:09:56 +0100

    Fixed some warnings

--- wp-content/mu-plugins/common/lib.php
+++ wp-content/mu-plugins/common/lib.php
@@ -46,7 +46,7 @@ add_action('do_meta_boxes', 'remove_page_meta_boxes');
 function set_order_meta_boxes($hidden, $screen) {
 	$post_type = $screen->post_type;
 	// So this can be used without hooking into user_register
-	if ( ! $user_id ) {
+	if ( ! isset($user_id) ) {
 		$user_id = get_current_user_id();
 	}
 
@@ -307,8 +307,8 @@ add_action('rss2_item', 'add_tags_rss');
  * Set number of posts per page for search and archive template
  * @author Xavier Meler (jmeler@xtec.cat)
  */
-function posts_per_page() {
-    if ( is_search() || is_archive() || is_author()){
+function posts_per_page ($query) {
+    if ( $query->is_search() || $query->is_archive() || $query->is_author()){
         set_query_var('posts_per_page', 10);
     }
 }
@@ -764,3 +764,16 @@ function add_flashvars_tinymc( $init ) {
     return $init;
 }
 add_filter('tiny_mce_before_init', 'add_flashvars_tinymc');
+
+/**
+ * Allow requests to some external hosts (like Gencat) to avoid problems with RSS (for instance)
+ *
+ * @author Sara Arjona Téllez
+ */
+function xtec_allowed_external_host( $allow, $host, $url ) {
+    if (strpos($host, '.gencat.cat') !== FALSE || strpos($host, '.xtec.cat') !== FALSE || strpos($host, '.edu365.cat') !== FALSE) {
+        $allow = true;
+    }
+    return $allow;
+}
+add_filter('http_request_host_is_external', 'xtec_allowed_external_host', 10, 3);
--- wp-content/themes/fukasawa/content.php
+++ wp-content/themes/fukasawa/content.php
@@ -43,6 +43,7 @@
 <!--//XTEC ************ AFEGIT - Added tags and categories information
 //2016.02.04 @sarjona -->
 		<?php
+			$output = '';
 			$categories = get_the_category();
 			foreach($categories as $category) {
                 $output .= '<a href="'.get_category_link( $category->term_id ).'" title="' . esc_attr( sprintf( __( "View all posts in %s" ), $category->name ) ) . '">'.$category->cat_name.'</a>'.", ";
