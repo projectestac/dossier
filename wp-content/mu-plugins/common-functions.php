4292e16d77c7fe684195aa4adeda711001d292c9
Sara Arjona <sarjona@xtec.cat>
2016-12-27 11:23:14 +0100

    Updated from parent 20161227

--- wp-content/mu-plugins/common-functions.php
+++ wp-content/mu-plugins/common-functions.php
@@ -10,8 +10,4 @@ Author: Àrea TAC - Departament d'Ensenyament de Catalunya
 require_once dirname(__FILE__) . '/common/lib.php';
 
 load_muplugin_textdomain('common-functions', '/common/languages');
-
-function xtec_enqueue_style () {
-	wp_enqueue_style('common-functions', get_site_url() . '/wp-content/mu-plugins/common/styles/common-functions.css');
-}
-add_action( 'wp_enqueue_scripts', 'xtec_enqueue_style' );
+wp_enqueue_style('common-functions', get_site_url() . '/wp-content/mu-plugins/common/styles/common-functions.css');
